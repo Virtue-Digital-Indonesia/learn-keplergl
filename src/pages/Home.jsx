e5ecac0addb9c3ea85114afa2dba7db52eabab17
dekguh <kdkteguhmahesa@gmail.com>
2022-12-30 11:06:21 +0800

    inject custom component into keplerGL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import KeplerGl from 'kepler.gl'
 import theme from '../utils/theme'
 import CustomToolbar from '../components/CustomToolbar'
+import { injectComponents, PanelHeaderFactory } from 'kepler.gl/dist/components'
+
+
+const CustomPanelHeader = () => (<div style={{ color: 'white', padding: '10px' }}>Custom Header</div>)
+const CustomPanelHeaderFactory = () => CustomPanelHeader
+
+// Inject custom component into Kepler.gl,
+const KeplerGl = injectComponents([
+  [PanelHeaderFactory, CustomPanelHeaderFactory]
+])
 
 const Home = () => {
   return (
