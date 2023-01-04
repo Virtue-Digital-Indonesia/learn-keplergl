27d9e1efb83fc5ba98b4eca7ca7be0c12169ed16
dekguh <kdkteguhmahesa@gmail.com>
2023-01-04 14:21:59 +0800

    implement spec modal import data

--- src/customFactory/CustomLoadDataModalFactory.jsx
+++ src/customFactory/CustomLoadDataModalFactory.jsx
@@ -1,13 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import { LoadDataModalFactory } from 'kepler.gl/components'
 import { LOADING_METHODS } from 'kepler.gl/dist/constants/default-settings'
 import { useDispatch } from 'react-redux'
 import { addDataToMap } from 'kepler.gl/dist/actions/actions'
-import evidence from '../utils/evidence.json'
+import exDataEvidence from '../utils/exDataEvidence'
+import sampleTripData from '../utils/sampleTripData'
+import sampleGpsData from '../utils/sampleGpsData'
+import { getProcessor } from '../utils'
+import sampleIconCsv, { config as configIconCsv } from '../utils/sampleIconCsv'
+import sampleS2csv, { config as configS2csv } from '../utils/sampleS2csv'
+import sampleSmallGeojson from '../utils/sampleSmallGeojson'
 
 const LoadViaURL = () => {
   const dispatch = useDispatch()
   const [loadUrl, setLoadUrl] = useState()
+  const [label, setLabel] = useState('')
+  const [type, setType] = useState('geojson')
 
   const fetchData = async (url) => {
     if(!url) return
@@ -18,51 +27,90 @@ const LoadViaURL = () => {
     })
     const result = await response.json()
 
-    dispatch(addDataToMap({
+    const params = {
       datasets: {
         info: {
-          label: 'COVID-19',
-          id: 'covid19'
+          label,
+          id: label,
         },
-        data: result
-      },
-      option: {
-        centerMap: true,
-        readOnly: false
-      },
-      config: {}
-    }))
-
+        data: getProcessor(type, result),
+      }
+    }
+    dispatch(addDataToMap(params))
     controller.abort()
   }
 
   return(
     <div>
-      <input type='text' placeholder='https://url' onChange={(event) => setLoadUrl(event.target.value)}/>
+      <div style={{ marginBottom: '8px' }}>
+        <input type='text' placeholder='label' onChange={(event) => setLabel(event.target.value)} />
+      </div>
+
+      <div style={{ marginBottom: '8px' }}>
+        <input type='text' placeholder='https://url' onChange={(event) => setLoadUrl(event.target.value)} />
+      </div>
+
+      <div style={{ marginBottom: '8px' }}>
+        <select onChange={event => setType(event.target.value)}>
+          <option value='geojson'>geojson</option>
+          {/* <option value='csv'>csv</option> */}
+          <option value='row'>row</option>
+        </select>
+      </div>
+
       <button onClick={() => fetchData(loadUrl)}>load</button>
     </div>
   )
 }
 
-const LoadViaDB = () => {
+const LoadViaSampleData = () => {
   const dispatch = useDispatch()
 
+  const handleOnClick = (label, id, inputData, type, config) => {
+    const params = {
+      datasets: {
+        info: {
+          label,
+          id,
+        },
+        data: type ? getProcessor(type, inputData) : inputData,
+        config: config ? config : {}
+      }
+    }
+    dispatch(addDataToMap(params))
+  }
+
   return(
-    <div>
-      <button onClick={() => dispatch(addDataToMap({
-        datasets: {
-          info: {
-            label: 'EVIDENCE',
-            id: 'evidence'
-          },
-          data: evidence
-        },
-        option: {
-          centerMap: true,
-          readOnly: false
-        },
-        config: {}
-      }))}>add evidence</button>
+    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', flexWrap: 'wrap' }}>
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Example point evidence</h3>
+        <button onClick={() => handleOnClick('evidence', 'evidence', exDataEvidence, null)}>try sample</button>
+      </div>
+
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Sample trip data</h3>
+        <button onClick={() => handleOnClick('tripdata', 'tripdata', sampleTripData, 'geojson')}>try sample</button>
+      </div>
+
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Sample gps data</h3>
+        <button onClick={() => handleOnClick('gps', 'gps', sampleGpsData, 'csv')}>try sample</button>
+      </div>
+
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Sample icon csv data</h3>
+        <button onClick={() => handleOnClick('iconcsv', 'iconcsv', sampleIconCsv, 'csv', configIconCsv)}>try sample</button>
+      </div>
+
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Sample s2 data</h3>
+        <button onClick={() => handleOnClick('s2csv', 's2csv', sampleS2csv, 'csv', configS2csv)}>try sample</button>
+      </div>
+
+      <div style={{ padding: '8px', width: '25%' }}>
+        <h3>Sample small geojson data</h3>
+        <button onClick={() => handleOnClick('smallgeo', 'smallgeo', sampleSmallGeojson, 'geojson')}>try sample</button>
+      </div>
     </div>
   )
 }
@@ -84,9 +132,9 @@ const CustomLoadDataModalFactory = (...deps) => {
         elementType: () => <LoadViaURL /> // props available
       },
       {
-        id: 'viadb',
-        label: 'Via Database',
-        elementType: () => <LoadViaDB /> // props available
+        id: 'sampledata',
+        label: 'Sample Data',
+        elementType: () => <LoadViaSampleData /> // props available
       }
     ]
   }
