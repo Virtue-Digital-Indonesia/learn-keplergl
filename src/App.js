d2fe8805c39e7e42822b6fdff32a4c574120b133
dekguh <kdkteguhmahesa@gmail.com>
2023-01-05 10:31:59 +0800

    add MUI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import Home from './pages/Home'
 import { store } from './redux/store'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { StyledEngineProvider } from '@mui/material'
 
 function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <StyledEngineProvider injectFirst>
+        <Home />
+      </StyledEngineProvider>
+      
     </Provider>
   )
 }
